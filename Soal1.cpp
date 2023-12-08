5c946b73ce9815c909722dc41ccb00e9d96f5970
Fernando Hendra Imanuel Siahaan <111106136+codecat92@users.noreply.github.com>
2023-12-08 18:37:14 +0700

    Create Soal1.cpp
    Jawaban soal no 1

---
+++ Soal1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    char Karakter = 'A';
+    char* Universit4s = "UNSIA";
+    char Jurusan[15] = "Informatika";
+    string firstName = "Budi";
+
+    cout << Karakter << endl;
+    cout << Universit4s << endl;
+    cout << Jurusan << endl;
+    cout << firstName << endl;
+
+    return 0;
+}
