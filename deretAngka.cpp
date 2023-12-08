f29076c22da0be5bd4c13d96140429b04b8b426c
Fernando Hendra Imanuel Siahaan <111106136+codecat92@users.noreply.github.com>
2023-12-08 18:00:46 +0700

    Add files via upload
    Jawaban soal no 4

---
+++ deretAngka.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Mendeklarasikan variabel
+    int jumlahDeret;
+
+    // Meminta input dari pengguna
+    cout << "Masukkan Jumlah Deret: ";
+    cin >> jumlahDeret;
+
+    // Menampilkan pola deret
+    for (int i = 1; i <= jumlahDeret; ++i) {
+        // Menampilkan angka dari 1 hingga i
+        for (int j = 1; j <= i; ++j) {
+            cout << i;
+            // Menambahkan tanda '-' jika bukan elemen terakhir
+            if (j < i) {
+                cout << "-";
+            }
+        }
+        // Menambahkan tanda '+' setelah deret angka
+        cout << "+\n";
+    }
+
+    return 0;
+}
